8af2ece1299386552542caa0d32d820514a89801
Anjula Madusanka Perera <anjulaperera98@gmail.com>
2026-05-24 15:46:03 +0530

    fix(backend): fix cache-dependancy-path in CI workflow

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -3,9 +3,12 @@ module.exports = {
   testEnvironment: "node",
   testMatch: ["**/__tests__/**/*.test.ts"],
   forceExit: true,
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.test.json'
-    }
-  }
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.test.json",
+      },
+    ],
+  },
 };
